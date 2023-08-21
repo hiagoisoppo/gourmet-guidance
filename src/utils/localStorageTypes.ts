d2f96e9ee646bfd07bbde9fa9dad80f60088eeb7
Hiago Isoppo <hiagoisoppotrajano@gmail.com>
2023-08-21 19:46:49 -0300

    Implementando useLocalStorage para manipulação total do localStorage

---
+++ src/utils/localStorageTypes.ts
@@ -0,0 +1,46 @@
+export interface LocalStorageReturnTypes {
+  user: { email: string };
+  setUser: React.Dispatch<React.SetStateAction<{ email: string; }>>;
+  doneRecipes: DoneRecipesType[];
+  handleDoneRecipes: (recipe: DoneRecipesType, option: 'add' | 'remove') => void;
+  favoriteRecipes: FavoriteRecipesType[];
+  handleFavoriteRecipes: (recipe: FavoriteRecipesType, option: 'add' | 'remove') => void;
+  inProgressRecipes: InProgressRecipesType;
+  handleInProgressRecipes: (
+    id: string,
+    type: 'meals' | 'drinks',
+    ingredientsList: number[],
+    option: 'add' | 'remove'
+  ) => void;
+}
+
+export interface DoneRecipesType {
+  id: string;
+  type: 'meal' | 'drink';
+  nationality: string;
+  category: string;
+  alcoholicOrNot: 'alcoholic' | 'non alcoholic' | '';
+  name: string;
+  image: string;
+  doneDate: string;
+  tags: string[];
+}
+
+export interface FavoriteRecipesType {
+  id: string;
+  type: 'meal' | 'drink';
+  nationality: string;
+  category: string;
+  alcoholicOrNot: 'alcoholic' | 'non alcoholic' | '';
+  name: string;
+  image: string;
+}
+
+export interface InProgressRecipesType {
+  drinks: {
+    [id: string]: number[];
+  },
+  meals: {
+    [id: string]: number[];
+  },
+}
