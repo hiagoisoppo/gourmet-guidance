f2ed2637918d5c6073be3ab21514355bb0b7db70
Hiago Isoppo <hiagoisoppotrajano@gmail.com>
2023-08-21 20:00:04 -0300

    Implementando formulário de Login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,8 +1,49 @@
+import { useState } from 'react';
+import useLocalStorage from '../../hooks/useLocalStorage';
+
 function Login() {
+  const { setUser } = useLocalStorage();
+  const [inputUser, setInputUser] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <div>
-      <span>Login</span>
-    </div>
+    <form
+      onSubmit={ (e) => {
+        e.preventDefault();
+        setUser({ email: inputUser });
+      } }
+    >
+      <h1>Login</h1>
+
+      <label htmlFor="email">
+        <input
+          value={ inputUser }
+          data-testid="email-input"
+          id="email"
+          type="email"
+          placeholder="Email"
+          onChange={ (e) => setInputUser(e.target.value) }
+        />
+      </label>
+
+      <label htmlFor="data">
+        <input
+          value={ password }
+          data-testid="password-input"
+          id="password"
+          type="password"
+          placeholder="Password"
+          onChange={ (e) => setPassword(e.target.value) }
+        />
+      </label>
+
+      <button
+        type="submit"
+        data-testid="login-submit-btn"
+      >
+        Enter
+      </button>
+    </form>
   );
 }
 
