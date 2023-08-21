557c9e6da8af366014fbb6165528f93f4ea39996
Hiago Isoppo <hiagoisoppotrajano@gmail.com>
2023-08-21 20:23:12 -0300

    Definindo validação do formulário, e redirecionamento de rota

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,43 +1,71 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import useLocalStorage from '../../hooks/useLocalStorage';
 
 function Login() {
+  const navigate = useNavigate();
   const { setUser } = useLocalStorage();
-  const [inputUser, setInputUser] = useState('');
-  const [password, setPassword] = useState('');
+  const [userData, setUserData] = useState({
+    email: '',
+    password: '',
+  });
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  const handleValidateForm = (
+    userInfo: { email: string; password: string },
+  ) => {
+    const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/i;
+    const isEmailValid = emailRegex.test(userInfo.email);
+    const isPasswordValid = userInfo.password.length > 5;
+
+    if (isEmailValid && isPasswordValid) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  };
 
   return (
     <form
       onSubmit={ (e) => {
         e.preventDefault();
-        setUser({ email: inputUser });
+        setUser({ email: userData.email });
+        navigate('/meals');
       } }
     >
       <h1>Login</h1>
 
       <label htmlFor="email">
         <input
-          value={ inputUser }
+          value={ userData.email }
           data-testid="email-input"
           id="email"
           type="email"
           placeholder="Email"
-          onChange={ (e) => setInputUser(e.target.value) }
+          onChange={ (e) => {
+            setUserData((prevUserData) => ({ ...prevUserData, email: e.target.value }));
+            handleValidateForm({ ...userData, email: e.target.value });
+          } }
         />
       </label>
 
-      <label htmlFor="data">
+      <label htmlFor="password">
         <input
-          value={ password }
+          value={ userData.password }
           data-testid="password-input"
           id="password"
           type="password"
           placeholder="Password"
-          onChange={ (e) => setPassword(e.target.value) }
+          onChange={ (e) => {
+            setUserData((prevUserData) => (
+              { ...prevUserData, password: e.target.value }));
+            handleValidateForm({ ...userData, password: e.target.value });
+          } }
         />
       </label>
 
       <button
+        disabled={ isDisabled }
         type="submit"
         data-testid="login-submit-btn"
       >
