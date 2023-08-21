8e7d19ed1e55494a2ea738fccfe88212ea4de5d8
Hiago Isoppo <hiagoisoppotrajano@gmail.com>
2023-08-21 08:16:50 -0300

    Implementando Setup do Browser Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+
+import './index.css';
 
 ReactDOM
   .createRoot(document.getElementById('root') as HTMLElement)
-  .render(<App />);
+  .render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
