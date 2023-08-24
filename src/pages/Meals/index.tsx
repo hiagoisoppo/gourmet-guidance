d0a9addf437028820a1bb6c5072fde1e07d8d762
lucasrcfraga <lucasrcfraga@gmail.com>
2023-08-24 01:30:44 -0300

    Fix de problemas durante o merge e adaptação dos testes

--- src/pages/Meals/index.tsx
+++ src/pages/Meals/index.tsx
@@ -1,3 +1,4 @@
+import Footer from '../../components/Footer';
 import Header from '../../components/Header/index';
 
 function Meals() {
@@ -5,6 +6,7 @@ function Meals() {
     <div>
       <Header title="Meals" showSearch />
       <h1>Comidas</h1>
+      <Footer />
     </div>
   );
 }
