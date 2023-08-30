263f6f7c04c9fefd6b053d7e805d9ea2ae1b4478
Lucas Yamagutsi <lucas.yamagutsi1@gmail.com>
2023-08-30 20:01:31 -0300

    Botões de filtragem funcionais e implementação inicial do botão de compartilhar

--- src/components/DoneRecipes/index.tsx
+++ src/components/DoneRecipes/index.tsx
@@ -1,61 +1,93 @@
+import { useState } from 'react';
 import useLocalStorage from '../../hooks/useLocalStorage';
+import shareImg from '../../images/shareIcon.svg';
 
 function DoneRecipes() {
+  const [selectedFilter, setSelectedFilter] = useState('none');
+  const [linkHasBeenCopied, setLinkHasBeenCopied] = useState(false);
   const { doneRecipes } = useLocalStorage();
+  let filteredRecipes = doneRecipes;
+
+  const handleSelectedFilter = (newFilter: string) => {
+    setSelectedFilter(newFilter);
+  };
+
+  if (selectedFilter === 'meal') {
+    filteredRecipes = doneRecipes.filter((recipe) => recipe.type === 'meal');
+  } else if (selectedFilter === 'drinks') {
+    filteredRecipes = doneRecipes.filter((recipe) => recipe.type === 'drink');
+  }
+
   return (
-    (doneRecipes ?? []).map((recipe, index) => {
-      if (recipe.type === 'meal') {
-        return (
+    <>
+      <button
+        data-testid="filter-by-all-btn"
+        onClick={ () => handleSelectedFilter('none') }
+      >
+        All
+      </button>
+      <button
+        data-testid="filter-by-meal-btn"
+        onClick={ () => handleSelectedFilter('meal') }
+      >
+        Meals
+      </button>
+      <button
+        data-testid="filter-by-drink-btn"
+        onClick={ () => handleSelectedFilter('drinks') }
+      >
+        Drinks
+      </button>
+      <div>
+        {(filteredRecipes ?? []).map((recipe, index) => (
           <div key={ recipe.id }>
-            <button data-testid="filter-by-all-btn">All</button>
-            <button data-testid="filter-by-meal-btn">Meals</button>
-            <button data-testid="filter-by-drink-btn">Drinks</button>
             <img
-              src="recipe.image"
+              src={ recipe.image }
               alt="Imagem do Card"
               data-testid={ `${index}-horizontal-image` }
             />
             <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
-            <p data-testid={ `${index}-horizontal-top-text` }>
-              {`${recipe.nationality} - ${recipe.category}`}
-            </p>
-            <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
-            <ul>
-              { recipe.tags.split(',').map((tagName: string, tagIndex: number) => (
-                <li
-                  key={ tagIndex }
-                  data-testid={ `${index}-${tagName}-horizontal-tag` }
-                >
-                  { tagName }
-                </li>
-              )) }
-            </ul>
-            <button data-testid={ `${index}-horizontal-share-btn` }>
-              Compartilhar receita
-            </button>
-          </div>
-        );
-      }
-      return (
-        <div key={ recipe.id }>
-          <button data-testid="filter-by-all-btn">All</button>
-          <button data-testid="filter-by-meal-btn">Meals</button>
-          <button data-testid="filter-by-drink-btn">Drinks</button>
-          <img
-            src="recipe.image"
-            alt="Imagem do Card"
-            data-testid={ `${index}-horizontal-image` }
-          />
-          <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
-          <p data-testid={ `${index}-horizontal-top-text` }>{recipe.alcoholicOrNot}</p>
-          <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
-          <button data-testid={ `${index}-horizontal-share-btn` }>
-            Compartilhar receita
-          </button>
-        </div>
-      );
-    })
+            {recipe.type === 'meal' ? (
+              <>
+                <p data-testid={ `${index}-horizontal-top-text` }>
+                  {`${recipe.nationality} - ${recipe.category}`}
+                </p>
+                <ul>
+                  {Array.isArray(recipe.tags) && recipe.tags.length > 0
+                    && recipe.tags.map((tagName: string, tagIndex: number) => (
+                      <li
+                        key={ tagIndex }
+                        data-testid={ `${index}-${tagName}-horizontal-tag` }
+                      >
+                        {tagName}
+                      </li>
+                    ))}
+                </ul>
 
+              </>
+            ) : (
+              <p data-testid={ `${index}-horizontal-top-text` }>
+                {recipe.alcoholicOrNot}
+              </p>
+            )}
+            <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
+
+            {linkHasBeenCopied && <span>Link copied!</span>}
+            <button
+              data-testid={ `${index}-horizontal-share-btn` }
+              onClick={ async (e) => {
+                e.preventDefault();
+                window.navigator.clipboard.writeText(window.location.href);
+                setLinkHasBeenCopied(true);
+              } }
+            >
+              <img src={ shareImg } alt="Compartilhar" />
+            </button>
+
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
--- src/utils/generalTypes.ts
+++ src/utils/generalTypes.ts
@@ -2,16 +2,6 @@ export interface UserType {
   email: string;
 }
 
-export interface FavoriteRecipesType {
-  id: number;
-  type: 'meal' | 'drink';
-  nationality: string;
-  category: string;
-  alcoholicOrNot: 'alcoholic' | 'non alcoholic' | '';
-  name: string;
-  image: string;
-}
-
 export interface RecipeCardProps {
   path: '/meals' | '/drinks';
   id: string;
