cd0baf267180ccfa8467398651a1ab3a2f081384
Lucas Yamagutsi <lucas.yamagutsi1@gmail.com>
2023-08-31 16:08:45 -0300

    Implementação final do botão de compartilhar e redirecionamento no nome e imagem

--- src/components/DoneRecipes/index.tsx
+++ src/components/DoneRecipes/index.tsx
@@ -1,18 +1,27 @@
 import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import useLocalStorage from '../../hooks/useLocalStorage';
 import shareImg from '../../images/shareIcon.svg';
+
+// Erro no req. 45 e 46 (Não acham o data-testid do botão de compartilhar)
+// Erro no req. 49 (Não está redirecionando pela imagem)
 
 function DoneRecipes() {
   const [selectedFilter, setSelectedFilter] = useState('none');
   const [linkHasBeenCopied, setLinkHasBeenCopied] = useState(false);
   const { doneRecipes } = useLocalStorage();
+  const navigate = useNavigate();
   let filteredRecipes = doneRecipes;
 
   const handleSelectedFilter = (newFilter: string) => {
     setSelectedFilter(newFilter);
   };
 
-  if (selectedFilter === 'meal') {
+  const handleRedirect = (recipeType: string, recipeId: string) => {
+    navigate(`/${recipeType}s/${recipeId}`);
+  };
+
+  if (selectedFilter === 'meals') {
     filteredRecipes = doneRecipes.filter((recipe) => recipe.type === 'meal');
   } else if (selectedFilter === 'drinks') {
     filteredRecipes = doneRecipes.filter((recipe) => recipe.type === 'drink');
@@ -28,7 +37,7 @@ function DoneRecipes() {
       </button>
       <button
         data-testid="filter-by-meal-btn"
-        onClick={ () => handleSelectedFilter('meal') }
+        onClick={ () => handleSelectedFilter('meals') }
       >
         Meals
       </button>
@@ -41,12 +50,20 @@ function DoneRecipes() {
       <div>
         {(filteredRecipes ?? []).map((recipe, index) => (
           <div key={ recipe.id }>
-            <img
-              src={ recipe.image }
-              alt="Imagem do Card"
-              data-testid={ `${index}-horizontal-image` }
-            />
-            <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
+            <Link to={ `/${recipe.type}s/${recipe.id}` }>
+              <img
+                src={ recipe.image }
+                alt="Imagem do Card"
+                data-testid={ `${index}-horizontal-image` }
+                width={ 50 }
+              />
+            </Link>
+            <button
+              data-testid={ `${index}-horizontal-name` }
+              onClick={ () => handleRedirect(recipe.type, recipe.id) }
+            >
+              {recipe.name}
+            </button>
             {recipe.type === 'meal' ? (
               <>
                 <p data-testid={ `${index}-horizontal-top-text` }>
@@ -71,17 +88,20 @@ function DoneRecipes() {
               </p>
             )}
             <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
-
             {linkHasBeenCopied && <span>Link copied!</span>}
             <button
-              data-testid={ `${index}-horizontal-share-btn` }
               onClick={ async (e) => {
                 e.preventDefault();
-                window.navigator.clipboard.writeText(window.location.href);
+                window.navigator.clipboard.writeText(`http://localhost:3000/${recipe.type}s/${recipe.id}`); // window.location.href
                 setLinkHasBeenCopied(true);
               } }
             >
-              <img src={ shareImg } alt="Compartilhar" />
+              <img
+                src={ shareImg }
+                alt="Compartilhar"
+                data-testid={ `${index}-horizontal-share-btn` }
+
+              />
             </button>
 
           </div>
