c8ef034534b65ac6a01f2a07e38762db21e18d64
Hiago Isoppo <hiagoisoppotrajano@gmail.com>
2023-09-04 19:41:49 -0300

    Testando up da vercel4

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [svgr(), react()],
   test: {
     globals: true,
     environment: 'jsdom',
